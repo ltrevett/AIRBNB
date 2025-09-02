5b8e2c0a8cf8cb48939bdd0fa23838eb87c7ba17
Lucas Trevett <lucastrevett12@outlook.com>
2025-09-02 12:41:32 +0000

    src_listings model added

---
+++ models/src_listings.sql
@@ -0,0 +1,18 @@
+with raw_listings as
+(
+    select
+
+        id as listing_id,
+        name as listing_name,
+        listing_url,
+        room_type,
+        minimum_nights,
+        host_id,
+        price as price_str,
+        created_at,
+        updated_at
+
+    from AIRBNB.RAW.RAW_LISTINGS
+)
+
+select * from raw_listings

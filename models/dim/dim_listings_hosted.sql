4b4f0b3fa7669e3114e78411b0c2f909bebddc5c
Lucas Trevett <lucastrevett12@outlook.com>
2025-09-02 13:10:30 +0000

    dim_listings_hosted model added

---
+++ models/dim/dim_listings_hosted.sql
@@ -0,0 +1,13 @@
+with cte as
+(
+    select 
+
+        host_id,
+        coalesce(host_name,'Anonymous') as host_name,
+        is_superhost,
+        created_at,
+        updated_at
+    
+    from {{ ref("src_hosts") }}
+)
+select * from cte
